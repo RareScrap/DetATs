080d9386388d0552de576a66155ee009fb3143a9
RareScrap <rarescrap@users.noreply.github.com>
2019-09-01 19:08:21 +0800

    feat(core): Зависимость от таска "deobfuscateJar" зашита в сам плагин
    + Если задача не найдена или найдено несколько - юзер устанавливает зависимость в своем билдскрипте вручную

--- src/main/java/ru/rarescrap/depats/BasePlugin.java
+++ src/main/java/ru/rarescrap/depats/BasePlugin.java
@@ -2,7 +2,10 @@ package ru.rarescrap.depats;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 import org.gradle.api.logging.Logger;
+
+import java.util.ArrayList;
 
 public class BasePlugin implements Plugin<Project> {
 
@@ -10,9 +13,17 @@ public class BasePlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
+        LOGGER = project.getLogger();
         DepATsPluginExtension e = project.getExtensions().create("DepATs", DepATsPluginExtension.class, project);
         GetDepATsTask t = project.getTasks().create("getDepATs", GetDepATsTask.class);
-        //t.dependsOn("deobfuscateJar"); // TODO: Протестировать
+
+        ArrayList<Task> tasks = new ArrayList<>(project.getTasksByName("deobfuscateJar", false));
+        if (tasks.isEmpty()) LOGGER.error("Can't find \"deobfuscateJar\" task. Make sure you apply the ForgeGradle plugin.");
+        else if (tasks.size() > 1) LOGGER.warn("There are multiple \"deobfuscateJar\" tasks. Set \"getDepATs.dependsOn(deobfuscateJar)\" in buildscript by yourself.");
+        else {
+            Task deobfuscateJar = tasks.get(0);
+            deobfuscateJar.dependsOn(t);
+        }
 
         // Для проверки кастомных значений из билдскрипта
 //        project.afterEvaluate(project1 -> {
@@ -20,6 +31,5 @@ public class BasePlugin implements Plugin<Project> {
 //            System.out.println(e1.getDepATs());
 //        });
 
-        LOGGER = project.getLogger();
     }
 }
